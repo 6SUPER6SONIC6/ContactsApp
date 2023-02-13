af6b6a7189e9082b798677587b4c4736f0ac2a06
6supe6sonic6 <6super6sonic@gmail.com>
2023-02-13 21:07:34 +0200

    main functionality implemented: - add contact; - update contact;

--- app/src/main/java/com/supersonic/contactsapp/MainActivity.java
+++ app/src/main/java/com/supersonic/contactsapp/MainActivity.java
@@ -1,14 +1,224 @@
 package com.supersonic.contactsapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import Data.ContactsAppDatabase;
+import Model.Contact;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ContactsAdapter contactsAdapter;
+    private ArrayList<Contact> contactArrayList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private ContactsAppDatabase contactsAppDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.mainRecyclerView);
+        contactsAppDatabase = Room.databaseBuilder(getApplicationContext(), ContactsAppDatabase.class, "ContactsDB")
+                .build();
+
+        new GetAllContactsAsyncTask().execute();
+
+        contactsAdapter = new ContactsAdapter(this, contactArrayList, MainActivity.this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(contactsAdapter);
+
+        FloatingActionButton addContactButton = findViewById(R.id.floatingActionButton);
+
+        addContactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addAndEditContacts(false, null, -1);
+            }
+        });
+
+
+    }
+
+    public void addAndEditContacts(final boolean isUpdate, final Contact contact, final int position){
+
+        LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflater.inflate(R.layout.layout_add_contact, null);
+
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilderUserInput.setView(view);
+
+        TextView newContactTitle = view.findViewById(R.id.newContactTitle);
+        final EditText firstNameEditText = view.findViewById(R.id.firstNameEditText);
+        final EditText secondNameEditText = view.findViewById(R.id.secondNameEditText);
+        final EditText emailEditText = view.findViewById(R.id.emailAddressEditText);
+        final EditText phoneNumberEditText = view.findViewById(R.id.phoneEditText);
+
+        newContactTitle.setText(!isUpdate ? "New Contact" : "Edit Contact");
+
+        if (isUpdate && contact != null){
+            firstNameEditText.setText(contact.getFirstName());
+            secondNameEditText.setText(contact.getSecondName());
+            emailEditText.setText(contact.getEmail());
+            phoneNumberEditText.setText(contact.getPhoneNumber());
+        }
+
+        alertDialogBuilderUserInput.setCancelable(false)
+                .setPositiveButton(isUpdate ? "Update" : "Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                    }
+                });
+
+        final AlertDialog alertDialog = alertDialogBuilderUserInput.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(firstNameEditText.getText().toString())){
+                    Toast.makeText(MainActivity.this, "Enter First name", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (TextUtils.isEmpty(secondNameEditText.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Enter Second Name", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (TextUtils.isEmpty(emailEditText.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (TextUtils.isEmpty(phoneNumberEditText.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Enter Phone Number", Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
+                    alertDialog.dismiss();
+                }
+
+                if (isUpdate && contact !=null){
+                    updateContact(firstNameEditText.getText().toString(), secondNameEditText.getText().toString(), emailEditText.getText().toString(), phoneNumberEditText.getText().toString()
+                    , position);
+                }else {
+                    createContact(firstNameEditText.getText().toString(), secondNameEditText.getText().toString(), emailEditText.getText().toString(), phoneNumberEditText.getText().toString());
+                }
+            }
+        });
+
+    }
+
+    private void createContact(String firstName, String secondName, String email, String phoneNumber) {
+        new CreateContactAsyncTast().execute(new Contact(0, firstName, secondName, email, phoneNumber));
+    }
+
+    private void updateContact(String firstName, String secondName, String email, String phoneNumber, int position) {
+        Contact contact = contactArrayList.get(position);
+
+        contact.setFirstName(firstName);
+        contact.setSecondName(secondName);
+        contact.setEmail(email);
+        contact.setPhoneNumber(phoneNumber);
+
+        new UpdateContactAsyncTask().execute(contact);
+
+        contactArrayList.set(position, contact);
+    }
+
+    private void deleteContact(Contact contact, int position) {
+        contactArrayList.remove(position);
+        new DeleteContactAsyncTask().execute(contact);
+    }
+
+    private class GetAllContactsAsyncTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            contactArrayList.addAll(contactsAppDatabase.getContactDAO().getAllContacts());
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+
+            contactsAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private class CreateContactAsyncTast extends AsyncTask<Contact, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Contact... contacts) {
+
+            long id = contactsAppDatabase.getContactDAO().addContact(contacts[0]);
+
+            Contact contact = contactsAppDatabase.getContactDAO().getContact(id);
+
+            if (contact !=null){
+                contactArrayList.add(0, contact);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            contactsAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private class UpdateContactAsyncTask extends AsyncTask<Contact, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Contact... contacts) {
+            contactsAppDatabase.getContactDAO().updateContact(contacts[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            contactsAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private class DeleteContactAsyncTask extends AsyncTask<Contact, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Contact... contacts) {
+            contactsAppDatabase.getContactDAO().deleteContact(contacts[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            contactsAdapter.notifyDataSetChanged();
+        }
     }
 }
